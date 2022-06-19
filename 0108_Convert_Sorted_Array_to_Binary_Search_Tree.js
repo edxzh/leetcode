93cd5ac872b20797c89d982e33207f6eb6b51058
Edward Zhou <Edward_mjz@hotmail.com>
2022-06-20 08:43:46 +1000

    Convert Sorted Array to Binary Search Tree

---
+++ 0108_Convert_Sorted_Array_to_Binary_Search_Tree.js
@@ -0,0 +1,13 @@
+const sortedArrayToBST = (nums) => {
+  if (nums.length === 0) {
+    return null;
+  }
+
+  let mid = Math.floor((nums.length) / 2);
+  let root = new TreeNode(nums[mid]);
+
+  root.left = sortedArrayToBST(nums.slice(0, mid));
+  root.right = sortedArrayToBST(nums.slice(mid + 1));
+
+  return root;
+};
