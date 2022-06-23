b4b3953c106d17692cdbfab5b61eb9e192828a54
Edward Zhou <Edward_mjz@hotmail.com>
2022-06-23 10:20:13 +1000

    0141 Linked List Cycle

---
+++ 0141_Linked_List_Cycle.js
@@ -0,0 +1,15 @@
+const hasCycle = (head) => {
+  let fast = head;
+  let slow = head;
+
+  while (fast !== null && fast.next !== null) {
+    fast = fast.next.next;
+    slow = slow.next;
+
+    if (fast === slow) {
+      return true;
+    }
+  }
+
+  return false;
+};
