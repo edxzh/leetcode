c19b4f0d458e60c58d6ceefc41c5402041df3a00
Edward Zhou <Edward_mjz@hotmail.com>
2022-06-25 10:26:40 +1000

    Refactor

--- 0108_Convert_Sorted_Array_to_Binary_Search_Tree.js
+++ 0108_Convert_Sorted_Array_to_Binary_Search_Tree.js
@@ -3,8 +3,8 @@ const sortedArrayToBST = (nums) => {
     return null;
   }
 
-  let mid = Math.floor((nums.length) / 2);
-  let root = new TreeNode(nums[mid]);
+  const mid = Math.floor((nums.length) / 2);
+  const root = new TreeNode(nums[mid]);
 
   root.left = sortedArrayToBST(nums.slice(0, mid));
   root.right = sortedArrayToBST(nums.slice(mid + 1));
