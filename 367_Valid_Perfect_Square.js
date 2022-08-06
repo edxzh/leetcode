fc169afe6eff87c2d44608fa7b59579e671c6825
Edward Zhou <Edward_mjz@hotmail.com>
2022-08-07 08:59:05 +1000

    367 Valid Perfect Square

---
+++ 367_Valid_Perfect_Square.js
@@ -0,0 +1,18 @@
+const isPerfectSquare = (num) => {
+  let left = 1;
+  let right = num;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (mid * mid < num) {
+      left = mid + 1;
+    } else if (mid * mid > num) {
+      right = mid - 1;
+    } else {
+      return true;
+    }
+  }
+
+  return false;
+};
